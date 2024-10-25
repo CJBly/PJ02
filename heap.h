eb771fae37dc5b581c68794de9a53013b64f7681
CJBly <cjbly@asu.edu>
2024-10-25 16:07:44 -0700

    Update heap.h

--- heap.h
+++ heap.h
@@ -8,5 +8,6 @@ void insert(HEAP *pHeap, ELEMENT **V, int index);
 ELEMENT* extractMin(HEAP *pHeap, ELEMENT **V);
 void decreaseKey(HEAP *pHeap, ELEMENT **V, int index, double newKey);
 void printHeap(HEAP *pHeap, ELEMENT **V);
+void printArray(TAG_ELEMENT **V, int n);
 
 #endif
