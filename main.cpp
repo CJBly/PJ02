519bb704d9bfa5a3b2f4e2a77061669dc71eca22
CJBly <cjbly@asu.edu>
2024-10-25 15:54:31 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,32 +5,48 @@
 #include "heap.h"
 #include "data_structures.h"
 
+void printArray(ELEMENT **V, int n) {
+    if (!V) {
+        fprintf(stderr, "Error: array is NULL\n");
+        return;
+    }
+    for (int i = 1; i <= n; i++) {
+        printf("Index: %d, Key: %lf, Pos: %d\n", V[i]->index, V[i]->key, V[i]->pos);
+    }
+}
+
+void printHeap(HEAP *pHeap, ELEMENT **V) {
+    if (!pHeap) {
+        fprintf(stderr, "Error: heap is NULL\n");
+        return;
+    }
+    for (int i = 1; i <= pHeap->size; i++) {
+        printf("Index: %d, Key: %lf, Pos: %d\n", V[pHeap->H[i]]->index, V[pHeap->H[i]]->key, V[pHeap->H[i]]->pos);
+    }
+}
+
 int main(int argc, char **argv) {
     if (argc != 3) {
-        fprintf(stderr, "Usage: %s <I-File> <O-File>\n", argv[0]);
+        fprintf(stderr, "Usage: ./PJ2 <I-File> <O-File>\n<I-File> is the input file\n<O-File> is the output file\n");
         return 1;
     }
 
     FILE *inputFile, *outputFile;
     HEAP *pHeap = NULL;
     ELEMENT **V = NULL;
-    int n;
+    int n = 0;
     char instruction[100];
-    double key;
-
-    outputFile = fopen(argv[2], "w");
-    if (!outputFile) {
-        fprintf(stderr, "Error: cannot open file %s\n", argv[2]);
-        return 1;
-    }
+    int index;
+    double newKey;
 
     while (1) {
-        int res = nextInstruction(instruction, &n, &key);
-        
+        int res = nextInstruction(instruction, &index, &newKey);
+
         if (strcmp(instruction, "Stop") == 0) {
             fprintf(stdout, "Instruction: Stop\n");
             break;
         } else if (strcmp(instruction, "Read") == 0) {
+            fprintf(stdout, "Instruction: Read\n");
             inputFile = fopen(argv[1], "r");
             if (!inputFile) {
                 fprintf(stderr, "Error: cannot open file %s\n", argv[1]);
@@ -50,20 +66,69 @@ int main(int argc, char **argv) {
             pHeap->capacity = n;
             pHeap->size = 0;
             pHeap->H = (int *)malloc((n + 1) * sizeof(int));
-        }
-        else if (strcmp(instruction, "BuildHeap") == 0) {
+        } else if (strcmp(instruction, "PrintArray") == 0) {
+            fprintf(stdout, "Instruction: PrintArray\n");
+            printArray(V, n);
+        } else if (strcmp(instruction, "PrintHeap") == 0) {
+            fprintf(stdout, "Instruction: PrintHeap\n");
+            printHeap(pHeap, V);
+        } else if (strcmp(instruction, "BuildHeap") == 0) {
+            fprintf(stdout, "Instruction: BuildHeap\n");
             buildHeap(pHeap, V, n);
-        }
-        else if (strcmp(instruction, "Insert") == 0) {
-            insert(pHeap, V, n);
-        }
-        else if (strcmp(instruction, "ExtractMin") == 0) {
-            ELEMENT *minElement = extractMin(pHeap, V);
-            if (minElement) {
-                printf("Element with index %d and key %lf extracted\n", minElement->index, minElement->key);
+        } else if (strcmp(instruction, "Insert") == 0) {
+            fprintf(stdout, "Instruction: Insert %d\n", index);
+            if (index < 1 || index > n) {
+                fprintf(stderr, "Error: index out of bound\n");
+            } else if (V[index]->pos != 0) {
+                fprintf(stderr, "Error: V[%d] already in the heap\n", index);
+            } else {
+                insert(pHeap, V, index);
+                fprintf(stdout, "Element V[%d] inserted into the heap\n", index);
             }
+        } else if (strcmp(instruction, "ExtractMin") == 0) {
+            fprintf(stdout, "Instruction: ExtractMin\n");
+            if (!pHeap) {
+                fprintf(stderr, "Error: heap is NULL\n");
+            } else if (pHeap->size == 0) {
+                fprintf(stderr, "Error: heap is empty\n");
+            } else {
+                ELEMENT *minElement = extractMin(pHeap, V);
+                if (minElement) {
+                    fprintf(stdout, "Element with index %d and key %lf extracted from the heap\n", minElement->index, minElement->key);
+                }
+            }
+        } else if (strcmp(instruction, "DecreaseKey") == 0) {
+            fprintf(stdout, "Instruction: DecreaseKey %d %lf\n", index, newKey);
+            if (index < 1 || index > n || newKey >= V[index]->key) {
+                fprintf(stderr, "Error: invalid call to DecreaseKey\n");
+            } else if (V[index]->pos == 0) {
+                fprintf(stderr, "Error: V[%d] not in the heap\n", index);
+            } else {
+                decreaseKey(pHeap, V, index, newKey);
+            }
+        } else if (strcmp(instruction, "Write") == 0) {
+            fprintf(stdout, "Instruction: Write\n");
+            outputFile = fopen(argv[2], "w");
+            if (!outputFile) {
+                fprintf(stderr, "Error: cannot open file %s\n", argv[2]);
+                continue;
+            }
+            for (int i = 1; i <= n; i++) {
+                fprintf(outputFile, "%d %lf %d\n", V[i]->index, V[i]->key, V[i]->pos);
+            }
+            fclose(outputFile);
+        } else {
+            fprintf(stdout, "Warning: Invalid instruction\n");
         }
     }
-    fclose(outputFile);
+
+    // Free memory and clean up
+    for (int i = 1; i <= n; i++) {
+        free(V[i]);
+    }
+    free(V);
+    free(pHeap->H);
+    free(pHeap);
+
     return 0;
 }
